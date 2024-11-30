de780c7cee62e7cccfca042d30a33196bfc36c49
Bra1kett <odinzov2004@gmail.com>
2024-11-30 18:23:42 +0300

    Исправил ошибку в 4 лабе

--- golang/main.go
+++ golang/main.go
@@ -1,9 +1,13 @@
 package main
 
-import lab4 "isuct.ru/informatics2022/labs/4lab"
+import (
+	"fmt"
+
+	lab4 "isuct.ru/informatics2022/labs/4lab"
+)
 
 func main() {
-  
+
 	lab4.Executelab4()
 
 	fmt.Println("ОДИНЦОВ АЛЕКСАНДР МИХАЙЛОВИЧ")
